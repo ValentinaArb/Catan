db4bde40da7aafe9d01c2ec5f5904168eb0e94c3
Valentina <valen.arbarello@gmail.com>
2022-08-24 08:45:11 -0300

    Base juego

---
+++ TP Catán/clases.py
@@ -0,0 +1,15 @@
+class Asentamiento:
+    def __init__ (self, jugador):
+        self.jugador = jugador
+
+class Ciudad:
+    pass
+
+class Camino:
+     def __init__ (self, jugador):
+        self.jugador = jugador
+
+class Jugador:
+    def __init__(self, nombre, color):
+        self.nombre = nombre
+        self.color = color
